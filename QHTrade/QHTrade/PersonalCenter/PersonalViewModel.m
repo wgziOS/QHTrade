209ca88312cd3f815649b0aa3f02a92166146699
wgz <547256553@qq.com>
2017-06-01 11:32:17 +0800

    personal mainview

---
+++ QHTrade/QHTrade/PersonalCenter/PersonalViewModel.m
@@ -0,0 +1,13 @@
+//
+//  PersonalViewModel.m
+//  QHTrade
+//
+//  Created by 吴桂钊 on 2017/6/1.
+//  Copyright © 2017年 qihuo.RDTrade.com. All rights reserved.
+//
+
+#import "PersonalViewModel.h"
+
+@implementation PersonalViewModel
+
+@end
