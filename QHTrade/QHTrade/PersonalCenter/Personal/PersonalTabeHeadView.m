c4460ac65d8ca5f5e5e200b19db371dd12b5765c
wgz <547256553@qq.com>
2017-07-03 13:44:13 +0800

    新版个人中心

---
+++ QHTrade/QHTrade/PersonalCenter/Personal/PersonalTabeHeadView.m
@@ -0,0 +1,27 @@
+//
+//  PersonalTabeHeadView.m
+//  QHTrade
+//
+//  Created by 吴桂钊 on 2017/7/3.
+//  Copyright © 2017年 qihuo.RDTrade.com. All rights reserved.
+//
+
+#import "PersonalTabeHeadView.h"
+
+@interface PersonalTabeHeadView ()
+
+@end
+
+@implementation PersonalTabeHeadView
+
+/*
+ @property (nonatomic,strong) UIImageView *headImgView;
+ @property (nonatomic,strong) UILabel *nickNameLabel;
+ @property (nonatomic,strong) UILabel *IdLabel;
+ @property (nonatomic,strong) UIImageView *sexImgView;
+ @property (nonatomic,strong) UIImageView *diamondImgView;
+ @property (nonatomic,strong) UIButton *setButton;
+ @property (nonatomic,strong) UIView *lineView;
+*/
+
+@end
